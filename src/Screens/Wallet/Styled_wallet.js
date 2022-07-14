72e61ae9a370738d8f38845382c0d29922d49e48
AndreesaARodrigues <andressaalmeida.aluno@unipampa.edu.br>
2022-07-14 12:33:47 -0300

    commit

--- src/Screens/Wallet/Styled_wallet.js
+++ src/Screens/Wallet/Styled_wallet.js
@@ -16,13 +16,13 @@ export const PageTitle = styled.View `
 
   background: #0057FF;
   border: 2px solid #3793FF;
-  border-radius: 12px;
+  border-radius: 10px;
 `;
 
 export const Title = styled.Text `
   font-style: normal;
   font-weight: 600;
-  font-size: 20px;
+  font-size: 19px;
   color:#fff;
 
   margin-left: 22px;
@@ -32,21 +32,64 @@ export const Title = styled.Text `
 export const PageBalance = styled.View `
   display: flex;
   width: 95%;
-  height:107px;
+  height: 100px;
   left: 10px;
-  top: 80pX;
+  top: 26px;
   
   background: #FFF;
-  border-radius: 12px;
+  border-radius: 10px;
+`;
+
+export const BalanceTitle = styled.Text `
+  left: 28px;
+  top: 18px;
+  color: #787878;
+  font-size: 18px;
 `;
 
 export const Balance = styled.Text `
-  display: flex;
-  width: 139px;
-  height: 22px;
-  left: 30px;
-  top: 20px;
-
-  font-size: 18px;
+  left: 28px;
+  top: 19px;
+  color: #00CD6B;
+  font-size: 26px;
   
+  font-weight: 400;
+`;
+
+export const History = styled.Text `
+  display: flex;
+  top: 60px;
+  left: 18px;
+  font-size: 19px;
+  color: #0057FF
+`;
+
+export const ContainerHistory = styled.View `
+  display: flex;
+  width: 95%;
+  height: 90px;
+  left: 10px;
+  top: 78px;
+   
+  background: #0057FF;
+  border: 1.8px solid #FFFFFF;
+  border-radius: 10px;
+`;
+
+export const BalanceHistory = styled.Text `
+  top: 30px;
+  left: 60px;
+  font-size: 20px;
+  color: #FFF;
+  
+  font-weight: 600;
+`;
+
+export const DateHistory = styled.Text `
+  top: 6px;
+  left: 280px;
+  font-size: 16px;
+  color: #FFF;
+  
+  font-weight: 400;
 `;
