b93b1d51745fb0e58a7bd7f3de151453af8d2194
Gabriel Tremarin <gabrieltremarin02@gmail.com>
2022-07-13 21:03:26 -0300

    firebase_config

--- src/Screens/Login/Login.jsx
+++ src/Screens/Login/Login.jsx
@@ -5,8 +5,7 @@ import { LinearGradient } from 'expo-linear-gradient';
 import GoogleLogo from '../../../assets/icons/GoogleLogo';
 import FacebookLogo from '../../../assets/icons/FacebookLogo';
 import NoQueueLogo from '../../../assets/icons/AppLogo';
-import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider } from "firebase/auth";
+
 
 
 
@@ -88,16 +87,4 @@ const Login = () => {
 export default Login
 
 
-
-const firebaseConfig = {
-  apiKey: "AIzaSyDYrXm6MUj2GilfiF58bnpPshybl9ONrj4",
-  authDomain: "noqueue-a0393.firebaseapp.com",
-  projectId: "noqueue-a0393",
-  storageBucket: "noqueue-a0393.appspot.com",
-  messagingSenderId: "529737889881",
-  appId: "1:529737889881:web:5a130ec5db31a48f06d948"
-};
-
-const app = initializeApp(firebaseConfig);
-const provider = new GoogleAuthProvider();
 
--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,15 +1,16 @@
 import { initializeApp } from "firebase/app";
-import { getAuth, } from "firebase/auth";
-
+import { GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDYrXm6MUj2GilfiF58bnpPshybl9ONrj4",
-    authDomain: "noqueue-a0393.firebaseapp.com",
-    projectId: "noqueue-a0393",
-    storageBucket: "noqueue-a0393.appspot.com",
-    messagingSenderId: "529737889881",
-    appId: "1:529737889881:web:5a130ec5db31a48f06d948"
-  };
+  apiKey: "AIzaSyDYrXm6MUj2GilfiF58bnpPshybl9ONrj4",
+  authDomain: "noqueue-a0393.firebaseapp.com",
+  projectId: "noqueue-a0393",
+  storageBucket: "noqueue-a0393.appspot.com",
+  messagingSenderId: "529737889881",
+  appId: "1:529737889881:web:5a130ec5db31a48f06d948"
+};
 
-export const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
+const app = initializeApp(firebaseConfig);
+const provider = new GoogleAuthProvider();
+
+
